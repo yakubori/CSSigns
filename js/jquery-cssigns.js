4475794f22f14d7150ca865dbdc597d2b60d379b
Rick Yakubowski <richard@orpha-systems.com>
2013-02-02 18:31:24 -0500

    Proper data biding via jQuery's data method.

--- js/jquery-cssigns.js
+++ js/jquery-cssigns.js
@@ -1,67 +1,77 @@
 (function($) {
 
 	var methods = {
-		init: function(opts) {
-			console.log('initialized!');
-			console.log(this.position().top);
+		init: function(options) {
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('cssigns'),
+					prefix = 'cssigns_',
+					bigOverlay = $('<div/>', {id: prefix+'big_overlay'}),
+					errmsg = $('<span/>', {id: prefix+'errmsg'});
+
+				if(!data) {
+					$(this).data('cssigns', {
+						prefix: prefix,
+						bigOverlay: bigOverlay,
+						errmsg: errmsg
+					});
+				}
+				console.log('initialized!');
+			});
 		},
 		render: function(content) {
-			var pluginClass = 'cssigns_';
-			var pos = $(this).position();
-			var width = $(this).width();
-			var stopit = jQuery('<div/>', {
-				class: pluginClass+'stopit'
+			return this.each(function() {
+				var $this = $(this),
+					data = $this.data('cssigns');
+
+				var pos = $this.position();
+				var width = $this.width();
+				var stopit = jQuery('<div/>', {
+					class: data.prefix+'stopit'
+				});
+				$(this).after(stopit);
+				stopit.css('top', pos.top + 2);
+				stopit.css('left', pos.left + width + 10);
+			
+				// The bang.
+				var bang = jQuery('<span/>', {class: data.prefix+'stopbang'});
+				bang.text('!');
+				bang.css('top', stopit.position().top - 3);
+				bang.css('left', stopit.position().left + 6);
+				stopit.after(bang);
+
+				// overlay
+				var overlay = jQuery('<div/>', { class: data.prefix+'overlay'});
+				overlay.css({top: stopit.position().top, left: stopit.position().left});
+				$('body').append(overlay);
+
+				data.bigOverlay.append(data.errmsg);
+				$('body').append(data.bigOverlay);
+
+				data.errmsg.text(content);
+
+				overlay.mouseover(function(e) {
+					var X = e.pageX;
+					var Y = e.pageY;
+					data.bigOverlay.show();
+					data.errmsg.css('top', Y + 20);
+					// Determine centering...
+					if( ( data.errmsg.width() / $(document).width() ) < .85 ) {
+						data.errmsg.css(
+							'left',
+							( ($(document).width() / 2) - (data.errmsg.width() / 2) )
+						);
+					} else {
+						data.errmsg.css({left: (($(document).width() / 2) - (data.errmsg.width() / 2)) / 2, top: Y + 20});
+					}
+				});
+				overlay.mouseout(function(e) {
+					data.bigOverlay.hide();
+				});
 			});
-			$(this).after(stopit);
-			stopit.css('top', pos.top + 2);
-			stopit.css('left', pos.left + width + 10);
-		
-			// The bang.
-			var bang = jQuery('<span/>', {class: pluginClass+'stopbang'});
-			bang.text('!');
-			bang.css('top', stopit.position().top - 3);
-			bang.css('left', stopit.position().left + 6);
-			stopit.after(bang);
-
-			// overlay
-			var overlay = jQuery('<div/>', { class: pluginClass+'overlay'});
-			overlay.css({top: stopit.position().top, left: stopit.position().left});
-			$('body').append(overlay);
-
-			var bigOverlay = $('<div/>', {
-				id: pluginClass+'big_overlay'
-			});
-			var errmsg = $('<span/>', {
-				id: pluginClass+'errmsg'
-			});
-
-			bigOverlay.append(errmsg);
-			$('body').append(bigOverlay);
-
-			errmsg.text(content);
-
-			overlay.mouseover(function(e) {
-				var X = e.pageX;
-				var Y = e.pageY;
-				bigOverlay.show();
-				errmsg.css('top', Y + 20);
-				// Determine centering...
-				if( ( errmsg.width() / $(document).width() ) < .85 ) {
-					errmsg.css(
-						'left',
-						( ($(document).width() / 2) - (errmsg.width() / 2) )
-					);
-				} else {
-					errmsg.css({left: (($(document).width() / 2) - (errmsg.width() / 2)) / 2, top: Y + 20});
-				}
-			});
-			overlay.mouseout(function(e) {
-				bigOverlay.hide();
-			});
-
 		},
-		delete: function() {
-
+		remove: function() {
+			console.log('removed...');
 		}
 	};
 
