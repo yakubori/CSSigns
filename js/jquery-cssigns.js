795f79a146fa13b3a9b3a61c6514e484b6bd6ff2
Rick Yakubowski <richard@orpha-systems.com>
2013-02-03 06:54:38 -0500

    Format and style cleanup on these source files.

--- js/jquery-cssigns.js
+++ js/jquery-cssigns.js
@@ -1,122 +1,128 @@
 (function($) {
 
-	var methods = {
-		init: function(options) {
-			return this.each(function() {
-				var $this = $(this),
-					data = $this.data('cssigns'),
-					prefix = 'cssigns_',
-					stopit = $('<div/>', {
-						class: prefix+'stopit'
-					});
-					bang = $('<span/>', {class: prefix+'stopbang'}),
-					overlay = $('<div/>', { class: prefix+'overlay'}),
-					bigOverlay = $('<div/>', {id: prefix+'big_overlay'}),
-					errmsg = $('<span/>', {id: prefix+'errmsg'});
+  var methods = {
 
-				if(!data) {
-					$(this).data('cssigns', {
-						prefix: prefix,
-						stopit: stopit,
-						bang: bang,
-						overlay: overlay,
-						bigOverlay: bigOverlay,
-						errmsg: errmsg
-					});
-				}
-				console.log('initialized!');
-			});
-		},
-		render: function(content) {
-			return this.each(function() {
-				var $this = $(this),
-					data = $this.data('cssigns');
+    init: function(options) {
+      return this.each(function() {
+        var $this = $(this),
+        data = $this.data('cssigns'),
+        prefix = 'cssigns_',
+        stopit = $('<div/>', {
+          class: prefix+'stopit'
+        });
+        bang = $('<span/>', {class: prefix+'stopbang'}),
+        overlay = $('<div/>', { class: prefix+'overlay'}),
+        bigOverlay = $('<div/>', {id: prefix+'big_overlay'}),
+        errmsg = $('<span/>', {id: prefix+'errmsg'});
 
-				var pos = $this.position();
-				var width = $this.width();
-				var stopit = data.stopit;
-				
-				stopit.css('top', pos.top + 2);
-				stopit.css('left', pos.left + width + 10);
+        if(!data) {
+          $(this).data('cssigns', {
+            prefix: prefix,
+            stopit: stopit,
+            bang: bang,
+            overlay: overlay,
+            bigOverlay: bigOverlay,
+            errmsg: errmsg
+          });
+        }
+        console.log('initialized!');
+        });
+    },
+    
+    render: function(content) {
+      return this.each(function() {
+        var $this = $(this),
+        data = $this.data('cssigns');
 
-				$this.after(stopit);
-			
-				// The bang.
-				var bang = data.bang;
-				bang.text('!');
-				bang.css('top', stopit.position().top - 3);
-				bang.css('left', stopit.position().left + 6);
-				stopit.after(bang);
+        var pos = $this.position();
+        var width = $this.width();
+        var stopit = data.stopit;
 
-				// overlay
-				var overlay = data.overlay;
-				overlay.css({
-					top: stopit.position().top,
-					left: stopit.position().left
-				});
-				$('body').append(overlay);
+        stopit.css('top', pos.top + 2);
+        stopit.css('left', pos.left + width + 10);
 
-				var bigOverlay = data.bigOverlay.append(data.errmsg);
-				$('body').append(bigOverlay);
+        $this.after(stopit);
 
-				var errmsg = data.errmsg;
-				errmsg.text(content);
+        // The bang.
+        var bang = data.bang;
+        bang.text('!');
+        bang.css('top', stopit.position().top - 3);
+        bang.css('left', stopit.position().left + 6);
+        stopit.after(bang);
 
-				overlay.mouseover(function(e) {
-					var X = e.pageX;
-					var Y = e.pageY;
-					bigOverlay.show();
-					errmsg.css('top', Y + 20);
-					// Determine centering...
-					if( ( errmsg.width() / $(document).width() ) < .85 ) {
-						errmsg.css(
-							'left',
-							( ($(document).width() / 2) - (errmsg.width() / 2) )
-						);
-					} else {
-						errmsg.css({left: (($(document).width() / 2) - (errmsg.width() / 2)) / 2, top: Y + 20});
-					}
-				});
-				overlay.mouseout(function(e) {
-					bigOverlay.hide();
-				});
-			});
-		},
-		clear: function() {
-			return this.each(function() {
-				var $this = $(this),
-					data = $this.data('cssigns');
+        // overlay
+        var overlay = data.overlay;
+        overlay.css({
+          top: stopit.position().top,
+          left: stopit.position().left
+          });
+        $('body').append(overlay);
 
-				data.bigOverlay.remove();
-				data.overlay.remove();
-				data.bang.remove();
-				data.stopit.remove();
-				console.log('cleared');
-			});
-		},
-		destroy: function() {
-			return this.each(function() {
-				var $this = $(this),
-					data = $this.data('cssigns');
+        var bigOverlay = data.bigOverlay.append(data.errmsg);
+        $('body').append(bigOverlay);
 
-				$(window).unbind('.cssigns');
-				$this.cssigns('clear');
-				$this.removeData('cssigns');
+        var errmsg = data.errmsg;
+        errmsg.text(content);
 
-				console.log('removed...');
-			});
-		}
-	};
+        overlay.mouseover(function(e) {
+            var X = e.pageX;
+            var Y = e.pageY;
+            bigOverlay.show();
+            errmsg.css('top', Y + 20);
+            // Determine centering...
+            if( ( errmsg.width() / $(document).width() ) < .85 ) {
+            errmsg.css(
+              'left',
+              ( ($(document).width() / 2) - (errmsg.width() / 2) )
+              );
+            } else {
+              errmsg.css({left: (($(document).width() / 2) - (errmsg.width() / 2)) / 2, top: Y + 20});
+            }
+            });
+        overlay.mouseout(function(e) {
+            bigOverlay.hide();
+            });
+        });
+    },
+    
+    clear: function() {
+      return this.each(function() {
+        var $this = $(this),
+            data = $this.data('cssigns');
 
-	$.fn.cssigns = function(method) {
-		// Method calling logic (taken right from plugin documentation).
-	    if ( methods[method] ) {
-	      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	    } else if ( typeof method === 'object' || ! method ) {
-	      return methods.init.apply( this, arguments );
-	    } else {
-	      $.error( 'Method ' +  method + ' does not exist on jQuery.cssigns' );
-	    }
-	};
+        data.bigOverlay.remove();
+        data.overlay.remove();
+        data.bang.remove();
+        data.stopit.remove();
+        console.log('cleared');
+        });
+    },
+    
+    destroy: function() {
+      return this.each(function() {
+        var $this = $(this),
+            data = $this.data('cssigns');
+
+        $(window).unbind('.cssigns');
+        $this.cssigns('clear');
+        $this.removeData('cssigns');
+        console.log('removed...');
+        });
+    }
+
+  };
+
+  $.fn.cssigns = function(method) {
+
+    // Method calling logic (taken right from plugin documentation).
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.cssigns' );
+    }
+
+  };
 
 })(jQuery);
